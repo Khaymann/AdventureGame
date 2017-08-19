21b101ce6f9fa779db5d5f595f3ec3f8c08dfff1
Khaymann <31054222+Khaymann@users.noreply.github.com>
2017-08-19 16:45:47 -0500

    Add files via upload

---
+++ Adventure Game!/Adventure Game!/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Adventure_Game_
+{
+    public static class Game
+    {
+        //charcter name
+        static string CharacterName = "";
+
+        //print out game title and overview
+        public static void StartGame()
+        {
+            Console.WriteLine("Adventure Game!");
+            Console.WriteLine("Welcome the my first game!");
+            NameCharacter();
+        }
+
+        //ask player for name and svae it
+        static void NameCharacter()
+        {
+            Console.Write("Please Enter Your Name: ");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Welcome the my first game " + CharacterName + "!");
+        }
+        
+    }
+
+    class Item
+    {
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //Console title
+            Console.Title = "Adventure Game!";
+
+            Game.StartGame();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}  
